efd82852df3a057a841b20747ae5b829710fe0ff
KRWCLASSIC <classic.krw@gmail.com>
2026-08-28 20:07:37 +0200

    Fix 1.20.1 (Forge only..?)

--- src/main/java/com/mikolajkolek/fixaltgr/mixin/InputConstantsMixin.java
+++ src/main/java/com/mikolajkolek/fixaltgr/mixin/InputConstantsMixin.java
@@ -2,26 +2,28 @@ package com.mikolajkolek.fixaltgr.mixin;
 
 import com.mikolajkolek.fixaltgr.FixAltGr;
 import com.mojang.blaze3d.platform.InputConstants;
+//#if MC >= 12100
 import org.spongepowered.asm.mixin.Dynamic;
+//#endif
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Coerce;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(InputConstants.class)
 public class InputConstantsMixin {
-    @Dynamic
     @Inject(at = @At(value = "HEAD"), method = "isKeyDown(JI)Z", cancellable = true, require = 0)
-    private static void isKeyDownLong(long window, int code, CallbackInfoReturnable<Boolean> cir) {
+    private static void isKeyDown(long window, int code, CallbackInfoReturnable<Boolean> cir) {
         checkAltGr(code, cir);
     }
 
+//#if MC >= 12100
     @Dynamic
-    @Inject(at = @At(value = "HEAD"), method = "isKeyDown", cancellable = true, require = 0)
-    private static void isKeyDownWindow(@Coerce Object window, int code, CallbackInfoReturnable<Boolean> cir) {
+    @Inject(at = @At(value = "HEAD"), method = "isKeyDown(Lcom/mojang/blaze3d/platform/Window;I)Z", cancellable = true, require = 0)
+    private static void isKeyDownWindow(com.mojang.blaze3d.platform.Window window, int code, CallbackInfoReturnable<Boolean> cir) {
         checkAltGr(code, cir);
     }
+//#endif
 
     private static void checkAltGr(int code, CallbackInfoReturnable<Boolean> cir) {
         if (code != 341) return;
